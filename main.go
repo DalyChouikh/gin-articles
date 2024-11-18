cbba24ec186b53cc9b95829bb46fa8620f061737
DalyChouikh <chouikhdaly215@gmail.com>
2024-11-18 23:37:11 +0100

    Setting up the entry file of the application by creating the router and running it

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func showIndexPage(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"title": "Home Page",
+		},
+	)
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", showIndexPage)
+	router.Run(":8080")
+}
